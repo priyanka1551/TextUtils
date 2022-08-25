989012669173005ad1e873c926b6cc2246898998
Priyanka Sharma <priyankashar717@gmail.com>
2022-08-25 10:21:29 +0530

    Updates

---
+++ precache-manifest.eb40a38b7fbd4f050d1622ec9c837362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "085bee1a209d7d703ee8",
+    "url": "/TextUtils/static/css/main.d864caaf.chunk.css"
+  },
+  {
+    "revision": "085bee1a209d7d703ee8",
+    "url": "/TextUtils/static/js/main.085bee1a.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "1fb84335d4ce8f26b1b6",
+    "url": "/TextUtils/static/js/2.1fb84335.chunk.js"
+  },
+  {
+    "revision": "14506651abc0b1064f2d",
+    "url": "/TextUtils/static/js/runtime~main.14506651.js"
+  },
+  {
+    "revision": "81f4bd50ed0923aeb26d47a813106fbc",
+    "url": "/TextUtils/index.html"
+  }
+];
